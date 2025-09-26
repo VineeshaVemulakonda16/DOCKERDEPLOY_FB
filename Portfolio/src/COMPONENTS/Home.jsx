dcb59a2e2ff60b54b457dccf2e00e27888ffd0d4
VEMULAKONDA NAAGA DHANYA HIMA VINEESHA <2300031234@kluniversity.in>
2025-09-26 19:45:06 +0530

    Update Home.jsx

--- Portfolio/src/COMPONENTS/Home.jsx
+++ Portfolio/src/COMPONENTS/Home.jsx
@@ -4,7 +4,7 @@ const Home = ({ portfolio }) => (
   <section className="pt-24 min-h-screen flex flex-col justify-center items-center bg-white text-gray-800">
     <div className="container mx-auto px-6 text-center">
       <h1 className="text-5xl md:text-7xl font-extrabold mb-4 text-green-700">
-        Hello, I'm <span>{portfolio.username}</span>
+        Hellllllllooooooooo, I'm <span>{portfolio.username}</span>
       </h1>
       <p className="text-xl md:text-2xl max-w-2xl mx-auto opacity-90">
         {portfolio.bio || "A passionate developer specializing in web apps."}
